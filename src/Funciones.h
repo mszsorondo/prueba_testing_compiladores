3055383f3bcab6dfc177a1942deff0e3ec5d0b50
msorondo <msorondo@live.com.ar>
2020-09-21 12:19:04 -0300

    Agregué varios archivos declaratorios y definitorios para modularizar mas el codigo

---
+++ src/Funciones.h
@@ -0,0 +1,11 @@
+//
+// Created by mssor on 21/9/2020.
+//
+
+#ifndef SOLUCION_FUNCIONES_H
+#define SOLUCION_FUNCIONES_H
+
+#endif //SOLUCION_FUNCIONES_H
+#include "Meses.h"
+bool esBisiesto(Anio anio);
+int diasEnMes(int anio, int mes);
